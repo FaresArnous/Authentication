34e41e68752cbd6990f9b30a8877bbc6822f1d43
fares Arnous <Faris.Arnous@hotmail.com>
2025-04-13 08:58:13 +0300

    initlize sessions pakages

--- app.js
+++ app.js
@@ -1,23 +1,45 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
+const session = require("express-session");
+const mongodbStore = reqiure("connect-mongodb-session");
 
-const db = require('./data/database');
-const demoRoutes = require('./routes/demo');
+const db = require("./data/database");
+const demoRoutes = require("./routes/demo");
+const { url } = require("inspector");
+const database = require("./data/database");
+const { Collection } = require("mongodb");
+
+const MongoDBStore = mongodbStore(session);
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+const sessionStore = new MongoDBStore({
+  url: "localhost:27017",
+  databaseName: "auth",
+  collection: "sessions",
+});
 
-app.use(express.static('public'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
+
+app.use(
+  session({
+    secret: "super-secret", //we choose this name for testing only, in real life project we shloud use something more secure
+    resave: false, //if this was true it will create new session each time the user send a request.
+    saveUninitialized: false,
+    store: sessionStore,
+  })
+);
 
 app.use(demoRoutes);
 
-app.use(function(error, req, res, next) {
-  res.render('500');
-})
+app.use(function (error, req, res, next) {
+  res.render("500");
+});
 
 db.connectToDatabase().then(function () {
   app.listen(3000);
